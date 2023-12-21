86e265b76ad38ce9225121fa3e40c487cffc94eb
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 21:28:58 +0800

    move Period class to file

---
+++ Budget/Models/Period.cs
@@ -0,0 +1,25 @@
+﻿namespace Budget.Models;
+
+public class Period
+{
+    public Period(DateTime start, DateTime end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    private DateTime End { get; set; }
+    private DateTime Start { get; set; }
+
+    public int OverlappingDays(BudgetDto budgetDto)
+    {
+        var overlappingEnd = End < budgetDto.LastDay()
+            ? End
+            : budgetDto.LastDay();
+        var overlappingStart = Start > budgetDto.FirstDay()
+            ? Start
+            : budgetDto.FirstDay();
+
+        return (overlappingEnd - overlappingStart).Days + 1;
+    }
+}
--- Budget/Services/BudgetService.cs
+++ Budget/Services/BudgetService.cs
@@ -7,30 +7,6 @@ using Budget.Models;
 
 namespace Budget.Services;
 
-public class Period
-{
-    public Period(DateTime start, DateTime end)
-    {
-        Start = start;
-        End = end;
-    }
-
-    public DateTime End { get; private set; }
-    public DateTime Start { get; private set; }
-
-    public int OverlappingDays(BudgetDto budgetDto)
-    {
-        var overlappingEnd = End < budgetDto.LastDay()
-            ? End
-            : budgetDto.LastDay();
-        var overlappingStart = Start > budgetDto.FirstDay()
-            ? Start
-            : budgetDto.FirstDay();
-
-        return (overlappingEnd - overlappingStart).Days + 1;
-    }
-}
-
 public class BudgetService
 {
     private readonly IBudgetRepo _budgetRepo;
