64932a69cc95eb5390c4bd9a2255b8dfc4d24228
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 20:51:18 +0800

    rename method: GetOverlappingAmount()

--- Budget/Services/BudgetService.cs
+++ Budget/Services/BudgetService.cs
@@ -28,17 +28,17 @@ public class BudgetService
         var budgetDomainModel = new BudgetDomainModel(budgetDtos);
         if (start.Month != end.Month)
         {
-            var startAmount = budgetDomainModel.GetAmount(start,
-                                                          new DateTime(start.Year, start.Month, DateTime.DaysInMonth(start.Year, start.Month)));
+            var startAmount = budgetDomainModel.GetOverlappingAmount(start,
+                                                                     new DateTime(start.Year, start.Month, DateTime.DaysInMonth(start.Year, start.Month)));
 
-            var endAmount = budgetDomainModel.GetAmount(new DateTime(end.Year, end.Month, 1), end);
+            var endAmount = budgetDomainModel.GetOverlappingAmount(new DateTime(end.Year, end.Month, 1), end);
 
             var middleMonthAmount = budgetDtos.Where(o => Convert.ToInt32(o.YearMonth) > Convert.ToInt32(start.ToString("yyyyMM")) && Convert.ToInt32(o.YearMonth) < Convert.ToInt32(end.ToString("yyyyMM"))).Sum(o => o.Amount);
 
             return startAmount + endAmount + middleMonthAmount;
         }
 
-        return budgetDomainModel.GetAmount(start, end);
+        return budgetDomainModel.GetOverlappingAmount(start, end);
     }
 }
 
@@ -51,7 +51,7 @@ public class BudgetDomainModel
         _budgetDtos = budgetDtos;
     }
 
-    public decimal GetAmount(DateTime start, DateTime end)
+    public decimal GetOverlappingAmount(DateTime start, DateTime end)
     {
         var budgetDto = _budgetDtos.FirstOrDefault(x => x.YearMonth == start.ToString("yyyyMM"));
         if (budgetDto == null)
