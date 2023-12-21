6ad766e9354594d65b73d5dbf7f533f02f54f651
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 21:34:49 +0800

    inline variable, and remove unused class

--- Budget/Services/BudgetService.cs
+++ Budget/Services/BudgetService.cs
@@ -23,32 +23,8 @@ public class BudgetService
             return 0;
         }
 
-        var budgetDtos = _budgetRepo.GetAll();
-
         var period = new Period(start, end);
 
-        return budgetDtos.Sum(budgetDto => budgetDto.OverlappingAmount(period));
-    }
-}
-
-public class BudgetDomainModel
-{
-    private readonly List<BudgetDto> _budgetDtos;
-
-    public BudgetDomainModel(List<BudgetDto> budgetDtos)
-    {
-        _budgetDtos = budgetDtos;
-    }
-
-    public decimal GetOverlappingAmount(DateTime overlappingStart, DateTime overlappingEnd, List<BudgetDto> budgetDtos)
-    {
-        var budgetDto = budgetDtos.FirstOrDefault(x => x.YearMonth == overlappingStart.ToString("yyyyMM"));
-        if (budgetDto == null)
-        {
-            return 0;
-        }
-
-        var overlappingDays = (overlappingEnd - overlappingStart).Days + 1;
-        return budgetDto.DailyAmount() * overlappingDays;
+        return _budgetRepo.GetAll().Sum(budgetDto => budgetDto.OverlappingAmount(period));
     }
 }
