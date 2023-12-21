a7b35a0a17fe0d19dc28c1144568bd460e1830c7
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 21:31:44 +0800

    inline temp variables

--- Budget/Models/Period.cs
+++ Budget/Models/Period.cs
@@ -14,14 +14,12 @@ public class Period
     public int OverlappingDays(BudgetDto budgetDto)
     {
         var another = new Period(budgetDto.FirstDay(), budgetDto.LastDay());
-        var firstDay = another.Start;
-        var lastDay = another.End;
-        var overlappingEnd = End < lastDay
+        var overlappingEnd = End < another.End
             ? End
-            : lastDay;
-        var overlappingStart = Start > firstDay
+            : another.End;
+        var overlappingStart = Start > another.Start
             ? Start
-            : firstDay;
+            : another.Start;
 
         return (overlappingEnd - overlappingStart).Days + 1;
     }
